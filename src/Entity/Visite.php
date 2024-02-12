4f10b2e02dfaff057a51b0717f212d4e72a2599d
yoyom261 <116827654+yoyom261@users.noreply.github.com>
2024-02-12 16:56:12 +0200

    Phase 6 : ORM (création de la BDD et de la table 'visite'

---
+++ src/Entity/Visite.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VisiteRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VisiteRepository::class)]
+class Visite
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $ville = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $pays = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $datecreation = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $note = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $avis = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $tempmin = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $tempmax = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): static
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getPays(): ?string
+    {
+        return $this->pays;
+    }
+
+    public function setPays(string $pays): static
+    {
+        $this->pays = $pays;
+
+        return $this;
+    }
+
+    public function getDatecreation(): ?\DateTimeInterface
+    {
+        return $this->datecreation;
+    }
+
+    public function setDatecreation(?\DateTimeInterface $datecreation): static
+    {
+        $this->datecreation = $datecreation;
+
+        return $this;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(?int $note): static
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getAvis(): ?string
+    {
+        return $this->avis;
+    }
+
+    public function setAvis(?string $avis): static
+    {
+        $this->avis = $avis;
+
+        return $this;
+    }
+
+    public function getTempmin(): ?int
+    {
+        return $this->tempmin;
+    }
+
+    public function setTempmin(?int $tempmin): static
+    {
+        $this->tempmin = $tempmin;
+
+        return $this;
+    }
+
+    public function getTempmax(): ?int
+    {
+        return $this->tempmax;
+    }
+
+    public function setTempmax(?int $tempmax): static
+    {
+        $this->tempmax = $tempmax;
+
+        return $this;
+    }
+}
