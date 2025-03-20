0cc7c46e39555411e1776b2adbc50a799c1d1b4b
Yoel <Yoel@sarah>
2025-03-20 19:24:08 +0200

    Phase 4 : plusieurs pages

---
+++ src/Controller/VoyagesController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * Description of VoyagesController
+ *
+ * @author Yoel
+ */
+class VoyagesController extends AbstractController{
+    
+        /**
+     * @Route("/voyages", name="voyages")
+     * @return Response
+     */
+    public function index(): Response{
+        return $this->render("pages/voyages.html.twig");   
+        }
+}
